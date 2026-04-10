c08eec11af176ed52e1b698220bde6e546f11847
Valtosudo <valtsu.vaajalahti@gmail.com>
2026-04-10 13:43:10 +0300

    eka

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Omat tehtävät",
+    "version": "1.0",
+    "summary": "Yhdistetty näkymä käyttäjän omista tehtävistä kaikissa projekteissa",
+    "category": "Productivity",
+    "author": "Your Company",
+    "depends": ["project"],
+    "data": [
+        "views/my_tasks_menu.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
